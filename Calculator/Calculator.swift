8dad4ca07815735c143e9867acd9687285d56e30
Riad Mohamed <riaduk1@gmail.com>
2019-10-29 22:14:20 +0200

    Basic operations are now functional, decimal point supported.

---
+++ Calculator/Calculator.swift
@@ -0,0 +1,56 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Riad Mohamed on 10/29/19.
+//  Copyright © 2019 Riad Mohamed. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    // MARK:- Properties
+    var leftOperand: Double? = nil
+    var rightOperand: Double? = nil
+    var isFinished: Bool = false
+    var operation: Int = 0
+    var result: Double? = nil
+    
+    // MARK:- Methods
+    func getLeftOperand(_ text: String) {
+        self.leftOperand = Double(text)
+    }
+    
+    func getRightOperand(_ text: String) {
+        self.rightOperand = Double(text)
+    }
+    
+    func calculate() -> Double {
+        // 1: Division, 2: Multiplication, 3: Subtraction, 4: Addition
+        if let leftOP = leftOperand, let rightOP = rightOperand {
+            switch operation {
+                case 1:
+                result = leftOP / rightOP
+                case 2:
+                result = leftOP * rightOP
+                case 3:
+                result = leftOP - rightOP
+                case 4:
+                result = leftOP + rightOP
+                default:
+                result = leftOP + rightOP
+            }
+            return result ?? 6969
+        }
+        print("ERROR READING OPERANDS!!")
+        return 6969
+    }
+    
+    func setOperand(_ text: String) {
+        if leftOperand == nil {
+            leftOperand = Double(text)
+        } else if rightOperand == nil {
+            rightOperand = Double(text)
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,18 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     // MARK:- UI OUTLETS
-    @IBOutlet var calculatorKeypadButton: [UIButton]!
+    @IBOutlet var calculatorKeypadButtons: [UIButton]!
+    @IBOutlet weak var screenLabel: UILabel!
+    @IBOutlet weak var clearButton: UIButton!
     
+    // MARK:- Calculator Properties
+    var text: String = "" {
+        didSet {
+            screenLabel.text = text
+            if text.isEmpty {
+                clearButton.setTitle("AC", for: .normal)
+            } else {
+                clearButton.setTitle("C", for: .normal)
+            }
+        }
+    }
+    let calc = Calculator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        screenLabel.text = ""
+        clearButton.setTitle("AC", for: .normal)
         roundTheCorners()
     }
     
     // MARK:- UI SETUP FUNCTIONS
     func roundTheCorners() {
-        for button in calculatorKeypadButton {
+        for button in calculatorKeypadButtons {
             if button.currentTitle == "0" {
                 button.layer.cornerRadius = 50
                 button.clipsToBounds = true
@@ -33,6 +49,36 @@ class ViewController: UIViewController {
         }
     }
 
-
+    
+    // MARK:- Calculator IB Functions
+    @IBAction func divButtonTapped(_ sender: UIButton) {
+        calc.operation = 1
+        calc.setOperand(text)
+        text = ""
+    }
+    @IBAction func multButtonTapped(_ sender: UIButton) {
+        calc.operation = 2
+        calc.setOperand(text)
+        text = ""
+    }
+    @IBAction func subButtonTapped(_ sender: UIButton) {
+        calc.operation = 3
+        calc.setOperand(text)
+        text = ""
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        calc.operation = 4
+        calc.setOperand(text)
+        text = ""
+    }
+    @IBAction func equalButtonTapped(_ sender: UIButton) {
+        calc.setOperand(text)
+        text = String(calc.calculate())
+    }
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        text.append(sender.currentTitle!)
+    }
+    
 }
 
