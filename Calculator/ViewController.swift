18b4e542b7a629a66ec8df0d276ab7c20d3fcf93
Riad Mohamed <riaduk1@gmail.com>
2019-10-29 16:51:38 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Riad Mohamed on 10/29/19.
+//  Copyright © 2019 Riad Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
